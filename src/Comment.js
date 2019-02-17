251d1b8a4a80788aba8546acd104f26ac86e0133
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-17 19:45:32 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,20 @@
 // add Comment component here
+
+import React, { Component } from 'react';
+
+export class EinsteinQuoteComponent extends Component {
+	render() {
+		return (
+			<div className="bar" id="einstein">
+				<i>
+					<p>
+						&quot;Life is like riding a bicycle. To keep your balance you must
+						keep moving&quot;
+					</p>
+				</i>
+				<br />
+				&#9;-Einstein
+			</div>
+		);
+	}
+}
